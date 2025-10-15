3b6fd443852a6ca38505d64e6b4c9789f4330454
parthgoyal527 <parthsherpur1550@gmail.com>
2025-10-15 11:47:17 +0530

    repo

---
+++ frontend/javascript/java.js
@@ -0,0 +1,28 @@
+
+// try{
+//     hello.touppercase();
+// } catch{
+//     console.log("ERROR");
+// }
+
+// function yell(msg){
+//     try{
+//     console.log(msg.touppercase().repeat(3));
+//     } catch (e){
+//         console.log(e);
+//         console.log("please enter the string");
+//     }
+// }
+
+
+// ARROW FUNCTION 
+const sum =(x,y) => {
+    return x+y;
+}
+
+const rollDie = () => (
+    Math.floor(Math.random() * 6) +1
+)
+
+//set time ou
+Movie.filter(m=>  )
